b209852eae6863e671c7bcd2ffbc76649ae0707c
nefera606 <garciademata@gmail.com>
2023-02-19 01:25:20 +0100

    Update deploy script - untested

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,9 @@
 const hre = require("hardhat");
 const fs = require('fs');
 
+const password = 'Passw0rd';
+const admin_keystore_file = '../accounts/admin-6e3976aeaa3a59e4af51783cc46ee0ffabc5dc11';
+
 const addresses = {};
 
 async function saveAddresesInfo(address, contractName, network) {
@@ -17,8 +20,51 @@ async function saveAddresesInfo(address, contractName, network) {
   console.log(`${contractName} address info saved!`);
 }
 
-async function main() {
+async function initialize(credentialRegistry, presentationRegistry, publicKeyRegistry, identityManager) {
+  const config = TruffleConfig.detect().env;
 
+  try {
+    console.log('Voy a petar');
+    let tx = await credentialRegistry.initialize(
+      '0x0000000000000000000000000000000000000001'
+    );
+    console.log(
+      `Credential registry initilized in ${tx.receipt.transactionHash}`
+    );
+
+    tx = await presentationRegistry.initialize(
+      '0x0000000000000000000000000000000000000001'
+    );
+    console.log(
+      `Presentation registry initilized in ${tx.receipt.transactionHash}`
+    );
+
+    tx = await publicKeyRegistry.initialize(
+      '0x0000000000000000000000000000000000000001'
+    );
+    console.log(
+      `Public key registry initilized in ${tx.receipt.transactionHash}`
+    );
+
+    tx = await identityManager.initialize(
+      addresses[config.credential],
+      addresses[config.publicKey],
+      addresses[config.presentation],
+      config.firstIdentityWallet
+    );
+    console.log(`Identity manager initilized in ${tx.receipt.transactionHash}`);
+
+  } catch (err) {
+    console.log('ERROR:', err);
+    callback(err, null);
+  }
+
+  console.log('Contracts initialized');
+};
+
+async function main() {
+  const admin_keystore = fs.readFileSync(admin_keystore_file);
+  const ADMIN_WALLET = new hre.ethers.Wallet.fromEncryptedJson(admin_keystore, password)
   const Eidas = await hre.ethers.getContractFactory("Eidas");
   // const AlastriaIdentityServiceProvider = await hre.ethers.getContractFactory("AlastriaIdentityServiceProvider");
   // const AlastriaIdentityIssuer = await hre.ethers.getContractFactory("AlastriaIdentityIssuer");
@@ -34,49 +80,50 @@ async function main() {
 
   const alastriaIdentityManager = await AlastriaIdentityManager.deploy();
   await alastriaIdentityManager.deployed();
-  const alastriaIdentityManagerProxy = await Proxy.deploy();
+  const alastriaIdentityManagerProxy = await Proxy.deploy(alastriaIdentityManager.address, ADMIN_ADDRESS, []);
   await alastriaIdentityManagerProxy.deployed();
   console.log('identityManager deployed: ', alastriaIdentityManagerProxy.address);
   await saveAddresesInfo(
     alastriaIdentityManagerProxy.address,
-    config.manager,
+    ADMIN_WALLET.address,
     network
   );
 
   const alastriaCredentialRegistry = await AlastriaCredentialRegistry.deploy();
   await alastriaCredentialRegistry.deployed();
-  const alastriaCredentialRegistryProxy = await Proxy.deploy();
+  const alastriaCredentialRegistryProxy = await Proxy.deploy(alastriaCredentialRegistry.address, ADMIN_ADDRESS, []);
   await alastriaCredentialRegistryProxy.deployed();
   console.log('identityManager deployed: ', alastriaIdentityManagerProxy.address);
   await saveAddresesInfo(
     alastriaCredentialRegistryProxy.address,
-    config.manager,
+    ADMIN_WALLET.address,
     network
   );
 
   const alastriaPresentationRegistry = await AlastriaPresentationRegistry.deploy();
   await alastriaPresentationRegistry.deployed();
-  const alastriaPresentationRegistryProxy = await Proxy.deploy();
+  const alastriaPresentationRegistryProxy = await Proxy.deploy(alastriaPresentationRegistry.address, ADMIN_ADDRESS, []);
   await alastriaPresentationRegistryProxy.deployed();
   console.log('identityManager deployed: ', alastriaIdentityManagerProxy.address);
   await saveAddresesInfo(
     alastriaPresentationRegistryProxy.address,
-    config.manager,
+    ADMIN_WALLET.address,
     network
   );
 
   const alastriaPublicKeyRegistry = await AlastriaPublicKeyRegistry.deploy();
   await alastriaPublicKeyRegistry.deployed();
-  const alastriaPublicKeyRegistryProxy = await Proxy.deploy();
+  const alastriaPublicKeyRegistryProxy = await Proxy.deploy(alastriaPublicKeyRegistry.address, ADMIN_ADDRESS, []);
   await alastriaPublicKeyRegistryProxy.deployed();
   console.log('identityManager deployed: ', alastriaIdentityManagerProxy.address);
   await saveAddresesInfo(
     alastriaPublicKeyRegistryProxy.address,
-    config.manager,
+    ADMIN_WALLET.address,
     network
   );
 
   await fs.writeFileSync('./addresses.json', JSON.stringify(addresses));
+  await initialize(alastriaCredentialRegistryProxy, alastriaPresentationRegistryProxy, alastriaPublicKeyRegistryProxy, alastriaIdentityManagerProxy);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
